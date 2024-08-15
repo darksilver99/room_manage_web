6c762f95f1d4117bf017e8d9e64d75b502117ae8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-15 04:45:40 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_model.dart
+++ lib/home_page/home_page_model.dart
@@ -4,6 +4,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'dart:math';
+import '/actions/actions.dart' as action_blocks;
 import 'home_page_widget.dart' show HomePageWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -13,6 +14,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  Local state fields for this page.
+
+  bool isLoading = true;
+
   @override
   void initState(BuildContext context) {}
 
