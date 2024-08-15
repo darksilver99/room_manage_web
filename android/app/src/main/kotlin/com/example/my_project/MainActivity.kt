42649391684a0d2ba6148778079126b69b9b0c16
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-15 03:21:00 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.silver.roommanageweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/silver/roommanageweb/room_manage_web/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.silver.roommanageweb.room_manage_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
